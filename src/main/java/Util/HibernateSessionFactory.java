fb495a4fcb23f349ac8574ca2b62312d35f4cadd
dorianJA <47135779+dorianJA@users.noreply.github.com>
2020-05-26 09:23:29 +0300

    Update HibernateSessionFactory.java

--- src/main/java/Util/HibernateSessionFactory.java
+++ src/main/java/Util/HibernateSessionFactory.java
@@ -24,7 +24,7 @@ public class HibernateSessionFactory {
         configuration.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
         configuration.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/test");
         configuration.setProperty("hibernate.connection.username", "root");
-        configuration.setProperty("hibernate.connection.password", "kazakdar");
+        configuration.setProperty("hibernate.connection.password", "******");
         configuration.setProperty("hibernate.show_sql", "true");
         configuration.setProperty("hibernate.hbm2ddl.auto", "create");
         return configuration;
